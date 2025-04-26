8a251c42c80c22f43cabfac1ab6200a69da025d5
Johnny Lin <hijohnnylin@gmail.com>
2025-04-25 18:09:54 -0700

    fix: exclude specific presets for explorable embed

--- apps/webapp/app/embed/sae-explorable/page.tsx
+++ apps/webapp/app/embed/sae-explorable/page.tsx
@@ -4,7 +4,7 @@ import SteererSimple from '../../../components/steer/steerer-simple';
 export default function Page() {
   return (
     <div className="flex h-full w-full flex-col items-center overflow-y-scroll bg-white px-0">
-      <SteererSimple initialModelId={DEFAULT_STEER_MODEL} />
+      <SteererSimple initialModelId={DEFAULT_STEER_MODEL} excludedPresetNames={['👎 Negativity', '㊗️ Chinese']} />
     </div>
   );
 }
