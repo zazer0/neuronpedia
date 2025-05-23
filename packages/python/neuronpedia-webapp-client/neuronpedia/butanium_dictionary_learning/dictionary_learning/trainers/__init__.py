464a53a8a06da1eab91f5ef2357f0631ae581b56
Johnny Lin <hijohnnylin@gmail.com>
2025-05-22 23:52:21 -0700

    webapp-library: simplify and remove dependencies

--- packages/python/neuronpedia-webapp-client/neuronpedia/butanium_dictionary_learning/dictionary_learning/trainers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .standard import StandardTrainer
-from .gdm import GatedSAETrainer
-from .p_anneal import PAnnealTrainer
-from .gated_anneal import GatedAnnealTrainer
-from .top_k import TrainerTopK
-from .jumprelu import TrainerJumpRelu
-from .batch_top_k import TrainerBatchTopK, BatchTopKSAE
-from .crosscoder import CrossCoderTrainer
