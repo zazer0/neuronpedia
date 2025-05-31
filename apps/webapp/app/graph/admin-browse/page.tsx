afe6d366464b345a3bfa5beb07dd89ee1a52477b
Johnny Lin <hijohnnylin@gmail.com>
2025-05-31 13:03:48 -0700

    new graph var

---
+++ apps/webapp/app/graph/admin-browse/page.tsx
@@ -0,0 +1,77 @@
+import { prisma } from '@/lib/db';
+import { GRAPH_ADMIN_BROWSE_KEY } from '@/lib/env';
+import { notFound } from 'next/navigation';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    key: string;
+  };
+}) {
+  // Check that the browse key is correct
+  if (searchParams.key !== GRAPH_ADMIN_BROWSE_KEY) {
+    notFound();
+  }
+
+  // get all metadatagraphs from the database after May 29th, 9am, 2025 pacific time
+  const allGraphMetadatas = await prisma.graphMetadata.findMany({
+    where: {
+      createdAt: {
+        gt: new Date('2025-05-29T16:00:00Z'),
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  return (
+    <div className="mx-auto max-w-screen-xl p-4">
+      <h1 className="mb-2 text-2xl font-bold">{allGraphMetadatas.length} Total Graphs Generated</h1>
+      <p className="mb-4 text-sm font-bold text-red-600">DO NOT SHARE</p>
+
+      <div className="overflow-x-auto">
+        <table className="min-w-full border border-gray-200 bg-white">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
+                Created At
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">Slug</th>
+              <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">Prompt</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200 bg-white">
+            {allGraphMetadatas.map((metadata) => (
+              <tr key={metadata.id}>
+                <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-900">
+                  {metadata.createdAt.toLocaleString()}
+                </td>
+                <td className="max-w-48 overflow-x-hidden truncate whitespace-nowrap px-6 py-4 text-sm font-medium text-sky-600">
+                  <a href={metadata.url} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                    {metadata.slug}
+                  </a>
+                </td>
+                <td className="px-6 py-4 text-sm text-gray-900">
+                  <div className="flex flex-col">
+                    <p className="mb-2">{metadata.prompt}</p>
+                    <div className="flex max-w-[600px] flex-wrap gap-x-1 gap-y-[3px]">
+                      {metadata.promptTokens?.map((t, idx) => (
+                        <span
+                          key={`${t}-${idx}`}
+                          className="mx-0 rounded bg-slate-200 px-[3px] py-[1px] font-mono text-[10px] text-slate-700"
+                        >
+                          {t}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
--- apps/webapp/lib/env.ts
+++ apps/webapp/lib/env.ts
@@ -131,3 +131,4 @@ export const NODE_ENV = process.env.NODE_ENV || '';
 export const IS_DOCKER_COMPOSE = process.env.IS_DOCKER_COMPOSE === 'true';
 export const DEMO_MODE = process.env.NEXT_PUBLIC_DEMO_MODE === 'true' || IS_ONE_CLICK_VERCEL_DEPLOY;
 export const ASSET_BASE_URL = 'https://neuronpedia.s3.us-east-1.amazonaws.com/site-assets';
+export const GRAPH_ADMIN_BROWSE_KEY = process.env.GRAPH_ADMIN_BROWSE_KEY || '';
